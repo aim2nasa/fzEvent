c3a34c1a5c7b684f88cd2ac533a9fdfc79b61fbf
skwak <skwak@4b5312f8-8f0e-ab43-9a09-d83ea11cb675>
2015-05-29 09:24:53 +0000

    서버가 안떠있고 포트 포워딩만 되어 있을때 서버는 연결된것으로 나온다. 이때는 쓸때만 오류가 나오는데 이경우 프로그램을 종료하게 한다

--- trunk/evtRcv/main.cpp
+++ trunk/evtRcv/main.cpp
@@ -35,12 +35,16 @@ int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 	er.activate();
 
 	for(int i=0;i<3;i++){
-		sendCmd(client_stream, CONTROL_MESSAGE_EVENT_RECORD_START);
+		if(sendCmd(client_stream, CONTROL_MESSAGE_EVENT_RECORD_START)==sizeof(unsigned int))
+			ACE_ERROR_RETURN((LM_ERROR,"(%P|%t) %p\n", "error sendCmd(CONTROL_MESSAGE_EVENT_RECORD_START)"),-1);
+
 		ACE_DEBUG((LM_DEBUG, "(%P|%t) EVENT Record start\n"));
 
 		ACE_OS::sleep(5);
 
-		sendCmd(client_stream, CONTROL_MESSAGE_EVENT_RECORD_STOP);
+		if (sendCmd(client_stream, CONTROL_MESSAGE_EVENT_RECORD_STOP) == sizeof(unsigned int))
+			ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p\n", "error sendCmd(CONTROL_MESSAGE_EVENT_RECORD_STOP)"), -1);
+
 		ACE_DEBUG((LM_DEBUG, "(%P|%t) EVENT Record stop\n"));
 		ACE_OS::sleep(1);
 	}
