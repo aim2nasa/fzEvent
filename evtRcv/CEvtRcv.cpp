2555c93da915eb6738415cf55d2c10e975fde50c
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-06-01 05:57:15 +0000

    unix_timeval_to_win32_systime 함수 추가

--- evtRcv/CEvtRcv.cpp
+++ evtRcv/CEvtRcv.cpp
@@ -35,6 +35,10 @@ int CEvtRcv::svc()
 		}
 		ACE_ASSERT(rcvSize==len);
 		ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%P|%t) Body %dbytes received\n"),rcvSize));
+
+		//convert unix timeval to SYSTEMTIME
+		SYSTEMTIME st;
+		unix_timeval_to_win32_systime(header.tv, &st);
 	}
 	return 0;
 }
@@ -50,4 +54,27 @@ _u32 CEvtRcv::parseHeader(device_packet_header* _header,char* pBuffer)
 	_u32 len = 0;
 	ACE_OS::memcpy(&len, p, sizeof(_u32));
 	return len;
+}
+
+void CEvtRcv::unix_timeval_to_win32_systime(const timeval& in, LPSYSTEMTIME st)
+{
+	LONGLONG ll;
+	FILETIME ft;
+
+	//#define USING_TIMEVAL_TO_SYSTIME_GMT_9
+#ifdef USING_TIMEVAL_TO_SYSTIME_GMT_9
+	{
+		ll = Int32x32To64(in.tv_sec + (60 * 60 * 9), 10000000) + 116444736000000000LL;
+	}
+#else /*USING_TIMEVAL_TO_SYSTIME_GMT_9*/
+		{
+			ll = Int32x32To64(in.tv_sec, 10000000) + 116444736000000000LL;
+		}
+#endif /*USING_TIMEVAL_TO_SYSTIME_GMT_9*/
+
+		ft.dwLowDateTime = (DWORD)ll;
+		ft.dwHighDateTime = ll >> 32;
+
+		FileTimeToSystemTime(&ft, st);
+		st->wMilliseconds = (WORD)(in.tv_usec / 1000);
 }
