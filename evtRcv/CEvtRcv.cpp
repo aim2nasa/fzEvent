761a40d977d8f156feb052c9b8f30ccd58f84227
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-06-01 05:35:04 +0000

    typedef포함 위치를 변경하고 parseHeader함수에서 len을 리턴하도록 변경하고 크기만큼 읽어들이도록 수정함

--- evtRcv/CEvtRcv.cpp
+++ evtRcv/CEvtRcv.cpp
@@ -15,28 +15,39 @@ CEvtRcv::~CEvtRcv()
 
 int CEvtRcv::svc()
 {
+	device_packet_header header;
 	char buf[BUFSIZ];
 	while (1){
-		ssize_t rcvSize = _pStream->recv(buf, sizeof(buf));
+		header.clear();
+		ssize_t rcvSize = _pStream->recv_n(buf, sizeof(header) + sizeof(_u32));
 		if (rcvSize <= 0) {
-			ACE_DEBUG((LM_ERROR, ACE_TEXT("(%P|%t) Connection closed\n")));
+			ACE_DEBUG((LM_ERROR, ACE_TEXT("(%P|%t) Connection closed while receiving header\n")));
 			return -1;
 		}
+		ACE_ASSERT(rcvSize == (sizeof(header) + sizeof(_u32)));
+		_u32 len = parseHeader(&header, buf);
+		ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%P|%t) Header %dbytes received, Len=%dbytes\n"), rcvSize,len));
 
-		ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%P|%t) %dbytes received\n"),rcvSize));
-
-		device_packet_header header;
-		header.clear();
-
-		parseHeader(&header, buf);
+		rcvSize = _pStream->recv_n(buf,len);
+		if (rcvSize <= 0) {
+			ACE_DEBUG((LM_ERROR, ACE_TEXT("(%P|%t) Connection closed while receiving body\n")));
+			return -1;
+		}
+		ACE_ASSERT(rcvSize==len);
+		ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%P|%t) Body %dbytes received\n"),rcvSize));
 	}
 	return 0;
 }
 
-void CEvtRcv::parseHeader(device_packet_header* _header,char* pBuffer)
+_u32 CEvtRcv::parseHeader(device_packet_header* _header,char* pBuffer)
 {
 	char* p = pBuffer;
 	ACE_OS::memcpy(&_header->tv, p, sizeof(_header->tv));
 	p += sizeof(_header->tv);
 	ACE_OS::memcpy(&_header->type, p, sizeof(_header->type));
+	p += sizeof(_header->type);
+
+	_u32 len = 0;
+	ACE_OS::memcpy(&len, p, sizeof(_u32));
+	return len;
 }
