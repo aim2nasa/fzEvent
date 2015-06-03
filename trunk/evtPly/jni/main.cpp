7770558bad3100f661d09b2a97f455502aa1c37e
skwak <skwak@4b5312f8-8f0e-ab43-9a09-d83ea11cb675>
2015-06-03 09:37:08 +0000

    generalize main

--- trunk/evtPly/jni/main.cpp
+++ trunk/evtPly/jni/main.cpp
@@ -3,6 +3,8 @@
 
 #include "ace/OS.h"
 
+#define DEFAULT_SLEEP 1
+
 int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 {
     ACE_TRACE(ACE_TEXT("main"));
@@ -10,25 +12,16 @@ int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
     ACE_LOG_MSG->priority_mask( LM_INFO|LM_ERROR, ACE_Log_Msg::PROCESS);
 
     if(argc<2)
-        ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT ("usage:ep <event file>\n")), -1);
+        ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT ("usage:ep <event file> <sleep between events in sec>\n")), -1);
 
     CEvtPlayer ep;
-    ep.read_event(argv[1]); 
-    ep.play_event(0);
-    ACE_OS::sleep(3);
-    ep.play_event(1);
-    ACE_OS::sleep(3);
-    ep.play_event(2);
-    ACE_OS::sleep(3);
-    ep.play_event(3);
-    ACE_OS::sleep(3);
-    ep.play_event(4);
-
-    ACE_OS::sleep(3);
-    ep.play_event(5);
-    ACE_OS::sleep(3);
-    ep.play_event(6);
-
+    int events = ep.read_event(argv[1]); 
+    ACE_DEBUG((LM_INFO,"(%P|%t) Readin events:%d\n",events));
+   
+    for(int i=0;i<events;i++) {
+        ep.play_event(i);
+        ACE_OS::sleep((argc>2)?ACE_OS::atoi(argv[2]):DEFAULT_SLEEP);
+    }        
     ACE_DEBUG((LM_INFO,"(%P|%t) event player end\n"));
     return 0;
 }
