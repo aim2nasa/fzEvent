6ce6c8a219387a5a8937d8e83fa12c026b99dfff
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-06-03 10:02:12 +0000

    modify displaying message about input values

--- evtPly/jni/main.cpp
+++ evtPly/jni/main.cpp
@@ -12,15 +12,19 @@ int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
     ACE_LOG_MSG->priority_mask( LM_INFO|LM_ERROR, ACE_Log_Msg::PROCESS);
 
     if(argc<2)
-        ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT ("usage:ep <event file> <sleep between events in sec>\n")), -1);
+        ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT ("usage:ep <event file> <[Opt]sleep(sec)>\n")), -1);
 
+    int sleep = (argc>2)?ACE_OS::atoi(argv[2]):DEFAULT_SLEEP;
+    ACE_DEBUG((LM_INFO,"(%P|%t) -event file:%s\n",argv[1]));
+    ACE_DEBUG((LM_INFO,"(%P|%t) -sleep(sec):%d\n",sleep));
+    
     CEvtPlayer ep;
     int events = ep.read_event(argv[1]); 
     ACE_DEBUG((LM_INFO,"(%P|%t) Readin events:%d\n",events));
    
     for(int i=0;i<events;i++) {
         ep.play_event(i);
-        ACE_OS::sleep((argc>2)?ACE_OS::atoi(argv[2]):DEFAULT_SLEEP);
+        ACE_OS::sleep(sleep);
     }        
     ACE_DEBUG((LM_INFO,"(%P|%t) event player end\n"));
     return 0;
