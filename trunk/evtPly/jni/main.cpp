2246bc3b835099f64ef95542aa5acb9193cab8f8
skwak <skwak@4b5312f8-8f0e-ab43-9a09-d83ea11cb675>
2015-06-03 02:58:49 +0000

    omitt LM_DEBUG severity

--- trunk/evtPly/jni/main.cpp
+++ trunk/evtPly/jni/main.cpp
@@ -7,7 +7,7 @@ int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 {
     ACE_TRACE(ACE_TEXT("main"));
 
-    ACE_LOG_MSG->priority_mask( LM_INFO|LM_ERROR|LM_DEBUG, ACE_Log_Msg::PROCESS);
+    ACE_LOG_MSG->priority_mask( LM_INFO|LM_ERROR, ACE_Log_Msg::PROCESS);
 
     if(argc<2)
         ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT ("usage:ep <event file>\n")), -1);
